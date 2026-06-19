ac0f605c7ef65e8a893e42a8a80a61457d7199f6
kimhuong25 <kimhuongg25114@gmail.com>
2026-06-19 22:28:12 +0700

    fix: lưu trữ lịch sử UserActivity

--- backend/models/UserActivity.js
+++ backend/models/UserActivity.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const userActivitySchema = new mongoose.Schema({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  action_type: { type: String, enum: ['search', 'view', 'borrow', 'favorite'], required: true },
+  
+  // ĐÃ BỔ SUNG TỪ KHÓA 'rate' VÀO MẢNG ENUM DƯỚI ĐÂY
+  action_type: { type: String, enum: ['search', 'view', 'borrow', 'favorite', 'rate'], required: true },
+  
   keyword: { type: String },
   book_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Book' }
 }, { timestamps: true });
